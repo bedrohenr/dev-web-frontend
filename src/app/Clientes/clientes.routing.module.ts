566fea6fbc80122dac639428d95a5e7311d6ff56
Thiago <thiagoffabiano@gmail.com>
2023-07-04 13:43:12 -0300

    feat: adiciona modal e rota para incluir clientes

--- src/app/Clientes/clientes.routing.module.ts
+++ src/app/Clientes/clientes.routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarClientesComponent } from './Listar/listar-clientes.component';
+import { IncluirClientesComponent } from './Incluir/incluir-clientes.component';
 export const clientesRoutes: Routes = [
   {
     path: '',
     component: ListarClientesComponent,
+    children: [
+      {
+        path:'Incluir',
+        component: IncluirClientesComponent
+      }
+    ]
   }
 ];
 
