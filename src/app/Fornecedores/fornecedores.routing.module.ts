047089a09d2f7ca2b2e379ef3c42980b2a6e6d68
Thiago <thiagoffabiano@gmail.com>
2023-07-05 22:51:40 -0300

    feat: adiciona rota para incluir fornecedores

--- src/app/Fornecedores/fornecedores.routing.module.ts
+++ src/app/Fornecedores/fornecedores.routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarFornecedoresComponent } from './Listar/listar-fornecedores.component';
+import { IncluirFornecedoresComponent } from './Incluir/incluir-fornecedores.component';
 export const fornecedoresRoutes: Routes = [
   {
     path: '',
     component: ListarFornecedoresComponent,
+    children: [
+      {
+        path: 'Incluir',
+        component: IncluirFornecedoresComponent
+      }
+    ]
   }
 ];
 
