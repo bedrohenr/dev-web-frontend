0d0378b21b0c85f1de7b4f5a144ede820c7e5f14
Thiago <thiagoffabiano@gmail.com>
2023-07-16 19:50:19 -0300

    feat(incluir-fornecedores): cria função para o submit

--- src/app/Fornecedores/fornecedores.module.ts
+++ src/app/Fornecedores/fornecedores.module.ts
@@ -3,6 +3,7 @@ import { FornecedoresRoutingModule } from './fornecedores.routing.module';
 import { CoreModule } from '../core/core.module';
 import { ListarFornecedoresComponent } from './Listar/listar-fornecedores.component';
 import { IncluirFornecedoresComponent } from './Incluir/incluir-fornecedores.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     exports: [
 
@@ -14,6 +15,7 @@ import { IncluirFornecedoresComponent } from './Incluir/incluir-fornecedores.com
     imports: [
       FornecedoresRoutingModule,
       CoreModule,
+      ReactiveFormsModule,
     ]
 })
 export class FornecedoresModule { }
