6b9deb4e0ccd5ccb4f70f5904736173dd1390c09
bedrohenr <ferfonhp@gmail.com>
2025-07-05 18:01:56 -0300

    feat: funcionalidade do form criação de bolão, nao finalizado

--- src/app/components/create-bolao/create-bolao.component.ts
+++ src/app/components/create-bolao/create-bolao.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-bolao',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-bolao.component.css']
 })
 export class CreateBolaoComponent {
-
   tipoResposta: string = "";
+
+  form: FormGroup;
+
+  constructor (
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.form = new FormGroup({
+      titulo: new FormControl('', [Validators.required]),
+      visibilidade: new FormControl('1', [Validators.required]),
+      descricao: new FormControl('', [Validators.required]),
+      tipoResposta: new FormControl('', [Validators.required]),
+      // logica para placar esportivo ou outro
+    })
+  }
+
+  onSubmit(){
+    if(this.form.invalid){
+      alert("Formulário invalido...");
+    } else {
+      console.log(this.form);
+    }
+  }
+
 }
