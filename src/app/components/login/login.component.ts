133a7d06bd3e4958cb0dc7d1c81715f6d607a933
bedrohenr <ferfonhp@gmail.com>
2025-07-05 16:02:16 -0300

    feat: funcionamento do formulario da tela de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor (
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit(){
+    if(this.form.invalid){
+      alert("Formulário invalido...");
+    } else {
+      console.log(this.form);
+    }
+  }
+
+  goSignUp(){
+    alert("SING UP");
+    this.router.navigate(['/sign-up']);
+  }
 }
