c096797b70e0b65a6850a6cf8cbc6a7d6dff127b
bedrohenr <ferfonhp@gmail.com>
2025-07-10 16:02:45 -0300

    feat: usuario service

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,30 @@
+// src/app/services/usuario.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Importe o HttpClient
+import { Observable } from 'rxjs'; // Para trabalhar com observables
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  // backend Node.js
+  private apiUrl = 'http://localhost:8000/api';
+  private apiCadastrarEndpoint = '/user/post-user';
+
+  constructor(private http: HttpClient) { } // Injete o HttpClient
+
+  /**
+   * Método para cadastrar um novo usuário.
+   * @param userData Os dados do usuário a serem cadastrados.
+   * @returns Um Observable com a resposta da API.
+   */
+  cadastrarUsuario(userData: any): Observable<any> {
+    const url = `${this.apiUrl}${this.apiCadastrarEndpoint}`;
+    return this.http.post<any>(url, userData);
+  }
+
+  // Você pode adicionar outros métodos aqui, como buscar, atualizar, deletar usuários, etc.
+  // getUsuarios(): Observable<any[]> {
+  //   return this.http.get<any[]>(this.apiUrl);
+  // }
+}
