5b3f4c4051e8debd80e423c84209bcb65fbc7d31
Thiago <thiagoffabiano@gmail.com>
2025-07-11 00:18:35 -0300

    feat: redirecionamento após login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { AuthInterceptor } from './../../core/handlers/http.handle-interceptor';
 import { AuthService } from './../../services/auth.service';
 import { UsuarioService } from './../../services/usuario.service';
+import { MensageriaService } from './../../core/mensageria/mensageria.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -20,7 +21,8 @@ export class LoginComponent {
     private route: ActivatedRoute,
     private router: Router,
     private usuarioService: UsuarioService,
-    private authService: AuthService
+    private authService: AuthService,
+    private mensageriaService: MensageriaService
   ) {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -37,14 +39,27 @@ export class LoginComponent {
         senha: this.form.value.senha
       };
 
+
        this.authService.login(dados).subscribe({
         next: (response) => {
           console.log('Usuário logado!', response);
           this.loginSucesso = true;
 
-          // Retornar a pagina login após criado
-          alert(response.message);
-          // this.router.navigate(['login'])
+          this.mensageriaService.mensagemSucesso('Login realizado com sucesso!');
+
+          const token = localStorage.getItem('access_token');
+
+          if (token) {
+            setTimeout(() => {
+              window.location.href = '/';
+            }, 1000);
+          } else {
+            this.router.navigate(['']).then((success) => {
+              if (!success) {
+                window.location.href = '/';
+              }
+            });
+          }
         },
         error: (error) => {
           console.error('Erro na tentativa de login:', error);
